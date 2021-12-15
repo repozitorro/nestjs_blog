7cacb8d689a3e2da8599bfb28977cc8e1e0d4593
repozitorro <repozitorro@gmail.com>
2021-12-15 17:04:11 +0200

    Add: dislike article

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -133,6 +133,29 @@ export class ArticleService {
     return article;
   }
 
+  async deleteArticleFromFavorites(
+    currentUserId: number,
+    slug: string,
+  ): Promise<ArticleEntity> {
+    const article = await this.findBySlug(slug);
+    const user = await this.userRepository.findOne(currentUserId, {
+      relations: ['favorites'],
+    });
+
+    const articleIndex = user.favorites.findIndex(
+      (articleInFavorites) => articleInFavorites.id === article.id,
+    );
+
+    if (articleIndex >= 0) {
+      user.favorites.splice(articleIndex, 1);
+      article.favoritesCount--;
+      await this.userRepository.save(user);
+      await this.articleRepository.save(article);
+    }
+
+    return article;
+  }
+
   buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
     return { article };
   }
