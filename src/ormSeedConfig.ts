72449b765b0324771d065d09c5f24b43a3698f47
repozitorro <repozitorro@gmail.com>
2021-12-16 17:18:12 +0200

    Add: auto generate data in database

---
+++ src/ormSeedConfig.ts
@@ -0,0 +1,11 @@
+import config from './ormconfig';
+
+const ormSeedConfig = {
+  ...config,
+  migrations: [__dirname + '/seeds/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/seeds',
+  },
+};
+
+export default ormSeedConfig;
---
+++ src/seeds/1639223907170-SeedDb.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SeedDb1639223907170 implements MigrationInterface {
+  name = 'SeedDb1639223907170';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO tags (name) VALUES ('dragons'), ('coffee'), ('nestjs')`,
+    );
+    await queryRunner.query(
+      `INSERT INTO users (username, email, password) VALUES ('admin', 'admin@gmail.com', '$2b$10$VOIJM32/JHOrRzzFPYlxT.EscKetuBlLAFcGNZA8S4Wu0sZ6CaDAO')`,
+    );
+    await queryRunner.query(
+      `INSERT INTO articles (slug, title, description, body, tagList, authorId) VALUES ('first-article', 'First article', 'First article desc', 'First article body', 'coffee,dragons', 1)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO articles (slug, title, description, body, tagList, authorId) VALUES ('second-article', 'second article', 'second article desc', 'second article body', 'second,dagon', 1)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO articles (slug, title, description, body, tagList, authorId) VALUES ('second-article24234', 'second article', 'second article desc', 'second article body', 'second,dagon', 1)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO articles (slug, title, description, body, tagList, authorId) VALUES ('second-article2wrew34', 'second article', 'second article desc', 'second article body', 'second,dagon', 1)`,
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public async down(): Promise<void> {}
+}
