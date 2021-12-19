fdb250eedebe0a49e598570b7b2b7cda9159cb4f
repozitorro <repozitorro@gmail.com>
2021-12-19 17:09:52 +0200

    Add: cors options

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
